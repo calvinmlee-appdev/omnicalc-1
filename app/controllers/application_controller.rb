2f006aef45a0f5f07c421853253d8e5e98702a61
calvinmlee <calvinmlee@uchicago.edu>
2023-01-26 02:24:58 +0000

    Squares finished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,21 @@ class ApplicationController < ActionController::Base
     render({ :template => "calculation_templates/square_form.html.erb"})
   end
 
+  def calculate_square
+
+    @num = params.fetch("elephant").to_f
+    @square_of_num = @num * @num
+    render({ :template => "calculation_templates/square_results.html.erb"})
+  end
+
+
+  def calculate_random
+
+    @lower = params.fetch("user_min").to_f
+    @upper = params.fetch("user_max").to_f
+
+    @result = rand(@lower..@upper)
+    render({ :template => "calculation_templates/random_results.html.erb"})
+  end
+
 end
